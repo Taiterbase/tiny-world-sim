50a21f990b6b36b5c181f9eddfeba6ef327cb934
Taite Nazifi <taitenazifi@gmail.com>
2022-01-07 06:12:08 -0700

    [component] created rudimentary map layout using MapProvider. Needs optimization

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,12 +1,26 @@
+import { MapPoint } from "models/map";
+import React, { memo, useEffect, useState } from "react";
+
 type CellProps = {
     x: number;
     y: number;
     island: boolean;
-}
-function Cell(props: CellProps) {
-    return (
-        <div className=""/>
-    )
+    onClick: (point: MapPoint) => Promise<void>;
+};
+
+const Cell = (props: CellProps) => {
+    const { x, y, island = false, onClick } = props;
+    const handleOnClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        onClick({x, y});
+    }
+    
+    useEffect(() => {
+        console.log(`Cell ${x}, ${y} was rerendered`)
+    })
+    
+    let bgColor = island ? `bg-yellow-150` : `bg-blue-640`;
+    let hoverColor = island ? `bg-blue-640` : `bg-yellow-150`;
+    return (<div key={`${x} ${y}`} onClick={(e) => handleOnClick(e)} className={`flex flex-grow ${bgColor} hover:${hoverColor} hover:shadow-lg`} />);
 }
 
-export default Cell;
+export default memo(Cell, ((prevProps, nextProps) => prevProps.island === nextProps.island));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,35 @@
+import Cell from "components/cell";
 import HomeLayout from "layouts/home"; // effectively getLayout from layouts/home
+import { MapBoundary } from "models/map";
+import { useMap } from "providers/map-provider";
+import { useEffect, useState } from "react";
 
 function HomePage(props: any) {
+    const world = useMap();
+
+    useEffect(() => {
+        console.log("world has updated!")
+    }, [world]);
+
+    const CellMap = () => {
+        return world.getMap()?.map((val, i) => {
+            return (<div key={`${i}`} className="flex flex-row flex-grow">
+                {(() => {
+                    return val.map((value, ii) => {
+                        return <Cell key={`${ii} ${i}`} onClick={world.setCell} x={ii} y={i} island={value} />
+                    })
+                })()}
+            </div>)
+        })
+    }
+
     return (
-        <div>
-            <p>Hey!</p>
-        </div>
+        <>
+            <div className="absolute top-10 right-10 w-10 h-10 bg-yellow-300 shadow-lg">
+                
+            </div>
+            {[...CellMap()]}
+        </>
     )
 }
 
