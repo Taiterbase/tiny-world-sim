406e35cbb8a2d4fc85e3a2b1b18a4ee6f5afb44b
Taite Nazifi <taitenazifi@gmail.com>
2022-01-06 18:46:20 -0700

    [model] created world map models for the 2d grid

---
+++ src/models/map/index.ts
@@ -0,0 +1,27 @@
+export type Map = boolean[][];
+
+export type MapPoint = {
+    x: number;
+    y: number;
+}
+
+export type MapSize = {
+    width: number;
+    height: number;
+}
+
+export type MapBoundary = {
+    north: number;
+    east: number;
+    south: number;
+    west: number;
+}
+
+export type MapProviderValues = {
+    getMap: () => Map;
+    getMapGridSize: () => MapSize;
+    setMapGridSize: (size: MapSize) => Promise<void>,
+    getMapBoundary: () => MapBoundary,
+    setCell: (cell: MapPoint) => Promise<void>,
+    getCell: (point: MapPoint) => boolean, 
+};
