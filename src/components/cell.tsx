e31f9bcb79f91a0ab0757324e19329f927bc16dd
Taite Nazifi <taitenazifi@gmail.com>
2022-01-06 18:45:18 -0700

    [component] created cell component

---
+++ src/components/cell.tsx
@@ -0,0 +1,12 @@
+type CellProps = {
+    x: number;
+    y: number;
+    island: boolean;
+}
+function Cell(props: CellProps) {
+    return (
+        <div className=""/>
+    )
+}
+
+export default Cell;
