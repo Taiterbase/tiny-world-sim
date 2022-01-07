20ce073c60c0e32f5e7b2b6924f1bfbf4d997e46
Taite Nazifi <taitenazifi@gmail.com>
2022-01-07 14:06:15 -0700

    [page] update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,12 @@
-import Cell from "components/cell";
-import HomeLayout from "layouts/home"; // effectively getLayout from layouts/home
-import { MapBoundary } from "models/map";
-import { useMap } from "providers/map-provider";
-import { useEffect, useState } from "react";
+import CellMap from "components/cell-map";
+import Header from "components/header";
+import HomeLayout from "layouts/home"; // getLayout fn from layouts/home
 
 function HomePage(props: any) {
-    const world = useMap();
-
-    useEffect(() => {
-        console.log("world has updated!")
-    }, [world]);
-
-    const CellMap = () => {
-        return world.getMap()?.map((val, i) => {
-            return (<div key={`${i}`} className="flex flex-row flex-grow">
-                {(() => {
-                    return val.map((value, ii) => {
-                        return <Cell key={`${ii} ${i}`} onClick={world.setCell} x={ii} y={i} island={value} />
-                    })
-                })()}
-            </div>)
-        })
-    }
-
     return (
         <>
-            <div className="absolute top-10 right-10 w-10 h-10 bg-yellow-300 shadow-lg">
-                
-            </div>
-            {[...CellMap()]}
+            <Header />
+            {CellMap()}
         </>
     )
 }
