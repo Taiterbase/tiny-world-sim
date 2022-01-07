69d8146a35f7e311a695907f81b38c6bc61521e9
Taite Nazifi <taitenazifi@gmail.com>
2022-01-06 18:45:56 -0700

    [type] typed react page param

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 
 import { RootLayout } from "layouts/root";
+import { ReactNode } from "react";
 
 export default function App({Component, pageProps}) {
-    const getLayout = Component.getLayout || ((page) => <RootLayout>{page}</RootLayout>);
+    const getLayout = Component.getLayout || ((page: ReactNode) => <RootLayout>{page}</RootLayout>);
     return getLayout(<Component {...pageProps} />);
 }
