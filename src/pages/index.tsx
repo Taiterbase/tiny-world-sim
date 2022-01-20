85712b31ffadea3b04e702dee264a41630dfcba3
Taite Nazifi <taitenazifi@gmail.com>
2022-01-19 23:29:58 -0700

    [feature] Toggle a loading page between load states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import CellMap from "components/cell-map";
 import Header from "components/header";
 import HomeLayout from "layouts/home"; // getLayout fn from layouts/home
+import { useMap } from "providers/map-provider";
 
 function HomePage(props: any) {
-    return (
-        <>
-            <Header />
-            {CellMap()}
-        </>
-    )
+    const world = useMap();
+
+    if (world.loading) {
+        return (
+            <>Loading!</>
+        )
+    } else {
+        return (
+            <>
+                <Header />
+                {CellMap()}
+            </>
+        )
+    }
 }
 
 HomePage.getLayout = HomeLayout;
